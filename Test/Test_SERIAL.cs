6b33d4850557022207cd07738955156c1d192098
cepthomas <cepthomas@gmail.com>
2025-03-11 17:27:56 -0500

    WIP.

--- Test/Test_SERIAL.cs
+++ Test/Test_SERIAL.cs
@@ -96,7 +96,7 @@ namespace NTerm.Test
         public int WriteBufferSize { get; set; }
         public int ReadTimeout { get; set; }
         public int WriteTimeout { get; set; }
-        public string PortName { get; set; }
+        public string PortName { get; set; } = "";
         public int BaudRate { get; set; }
         public Parity Parity { get; set; }
         public int DataBits { get; set; }
